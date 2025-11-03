f2846f5f2d1d394739ef6f129b9d32a3e9f79c9f
Juan Zahid Angeles Sarabia <zahidangeles11@gmail.com>
2025-11-02 22:56:00 -0800

    lets see

--- frontend/src/components/LoadingStatus.jsx
+++ frontend/src/components/LoadingStatus.jsx
@@ -1,13 +1,13 @@
-function LoadingStatus({theme}){
+function LoadingStatus({theme}) {
     return <div className="loading-container">
-        <h2>Generating your {theme} Story</h2>
+        <h2>Generating Your {theme} Story</h2>
 
         <div className="loading-animation">
             <div className="spinner"></div>
         </div>
 
         <p className="loading-info">
-            Please wait while we generate your story..
+            Please wait while we generate your story...
         </p>
     </div>
 }
